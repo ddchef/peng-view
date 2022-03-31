1a859bc9fafe15fa7acc5581629bc3ff1e216be5
ddchef <o.o@xjie.me>
2022-03-31 20:58:08 +0800

    feat: 用户管理添加查询接口

--- src/view/user/index.vue
+++ src/view/user/index.vue
@@ -1,23 +1,52 @@
 <template>
-  <n-data-table :columns="columns" :data="data"></n-data-table>
+  <data-table
+    :columns="columns"
+    :data="data"
+    :total="total"
+    :page="page"
+    :limit="limit"
+    @change-limit="onChangeLimit"
+    @change-page="onChangePage"
+  />
 </template>
 <script setup lang="ts">
-import { NDataTable, DataTableColumns } from 'naive-ui';
-import { reactive } from 'vue';
+import { DataTableColumns } from 'naive-ui';
+import { ref } from 'vue';
 import { getUserList } from './api';
+import DataTable from '../../components/dataTable/index.vue';
 
 interface User{
   id:number,
   username:string,
   mobile: string,
-  timestamps: string,
+  created_at: string,
+  updated_at: string,
 }
-const data = reactive<User[]>([]);
-getUserList<User>().then((res) => {
-  if (res.error_code === 0) {
-    console.log(res.data);
-  }
-});
+
+const total = ref(0);
+const page = ref(1);
+const data = ref<User[]>([]);
+const limit = ref(20);
+const getData = () => {
+  getUserList<{total:number, list:User[]}>(
+    { offset: (page.value - 1) * limit.value, limit: limit.value },
+  ).then((res) => {
+    if (res.error_code === 0) {
+      data.value = res.data.list;
+      total.value = res.data.total;
+    }
+  });
+};
+getData();
+const onChangePage = (v:number) => {
+  page.value = v;
+  getData();
+};
+const onChangeLimit = (v:number) => {
+  limit.value = v;
+  getData();
+};
+
 const columns:DataTableColumns<User> = [
   {
     key: 'id',
@@ -32,8 +61,12 @@ const columns:DataTableColumns<User> = [
     title: '手机号',
   },
   {
-    key: 'timestamps',
+    key: 'created_at',
     title: '创建时间',
+  },
+  {
+    key: 'updated_at',
+    title: '最后更新时间',
   },
 ];
 </script>
